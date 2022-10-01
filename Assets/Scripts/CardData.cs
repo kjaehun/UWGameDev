f181a60d9aad6d0c9473f91ee95f02e2dcdfc8e9
Yffriium <gbloxers@gmail.com>
2022-10-01 15:12:50 -0500

    test commit

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData
+{
+    int cost;
+    int attack;
+
+    public CardData() {
+        cost = 1;
+        attack = 5;
+    }
+}
